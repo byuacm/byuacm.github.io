70355db0b6d51b1474644f071fe2a4d77e5f5402
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-05-24 15:52:18 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -36,7 +36,11 @@ export default new Router({
       name: 'About',
       component: About,
     },
-
+    {
+      path: '/resources',
+      name: 'Resources',
+      component: Resources,
+    },
 
   ]
 })
