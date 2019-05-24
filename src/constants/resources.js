4743f0ed1fe66623d73529a9be4e052fc634da42
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-05-24 15:27:08 -0400

    Create resources.js

---
+++ src/constants/resources.js
@@ -0,0 +1,11 @@
+import * as moment from 'moment';
+
+const resources = [
+  {
+    title: 'How to Pass a Technical Interview',
+    description: 'Qualtrics presentation given in Winter 2019'
+    link: '../static/resources/qualtricsInterview.pdf'
+  },
+];
+
+export default resources;
