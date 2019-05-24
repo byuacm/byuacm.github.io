18f6ac1db8f9d5aac4ca8b9bb5a34196f9aa5e61
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-05-24 17:28:51 -0400

    Update resources.js

--- src/constants/resources.js
+++ src/constants/resources.js
@@ -1,11 +1,10 @@
 import * as moment from 'moment';
-import pdf from '../assets/mypdf.pdf';
 
 const resources = [
   {
     title: 'How to Pass a Technical Interview',
     description: 'Qualtrics presentation given in Winter 2019',
-    link: '../static/resources/qualtricsInterview.pdf',
+    link: 'static/resources/qualtricsInterview.pdf',
   },
 ];
 
