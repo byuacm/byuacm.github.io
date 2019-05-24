dc01a9627e8ed0c059caa9fc7f85e4dc7aa1d191
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-05-24 15:04:44 -0400

    Create resources.vue

---
+++ src/components/resources.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="resources-container">
+    <md-card class="resource" v-for="resource in resources" v-bind:key="resources.title">
+      <md-card-header>
+        <h4 class="md-title">{{resource.title}}</h4>
+      </md-card-header>
+      <md-card-content>
+        <div>{{resource.description}}</div>
+        <div>{{resource.link}}</div>
+      </md-card-content>
+    </md-card>
+  </div>
+</template>
+
+<script>
+import resources from "../constants/resources.js";
+import * as moment from 'moment';
+export default {
+  data() {
+    return {
+      resources
+    };
+  }
+};
+</script>
+
+<style scoped>
+.events-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: start;
+  align-items: center;
+  max-width: 1000px;
+  margin: 0 auto;
+}
+.events-title {
+  color: white;
+}
+.md-card {
+  margin-bottom: 30px;
+  width: 100%;
+}
+.md-subhead {
+  display: flex;
+  flex-direction: row;
+}
+.md-subhead div {
+  margin: 0 20px 0 5px;
+}
+.md-subhead {
+  display: flex;
+}
+</style>
