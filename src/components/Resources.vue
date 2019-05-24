74ac753187bf7226a6f6e7e0f9ad4a11a305a1d4
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-05-24 17:37:20 -0400

    Update Resources.vue

--- src/components/Resources.vue
+++ src/components/Resources.vue
@@ -4,7 +4,7 @@
       <md-card class="resource" v-for="resource in resources" v-bind:key="resource.title">
         <md-card-header>
           <h4 class="md-title">
-            <a href="resource.link" download>
+            <a v-bind:href="resource.link" download>
               {{resource.title}}
             </a>
           </h4>
