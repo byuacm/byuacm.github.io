71846723a7ae88f0830449066a183546aae6f626
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-05-24 15:44:52 -0400

    Update index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Events from '@/components/Events'
 import Leadership  from '@/components/Leadership'
 import Contributors from '@/components/Contributors'
 import About from '@/components/About'
+import Resources from '@/components/Resources'
 
 Vue.use(Router)
 
@@ -31,6 +32,11 @@ export default new Router({
       component: Contributors,
     },
     {
+      path: '/about',
+      name: 'About',
+      component: About,
+    },
+        {
       path: '/about',
       name: 'About',
       component: About,
