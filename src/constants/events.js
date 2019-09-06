efb3c8b19f09c50ab8216d73f187858b55efcee0
BYU ACM <42621273+byuacm-master@users.noreply.github.com>
2019-09-05 20:01:57 -0600

    Update events.js

--- src/constants/events.js
+++ src/constants/events.js
@@ -9,18 +9,18 @@ const events = [
     description: 'Our first event of the semester! Come listen to a presentation by Lucid to kick of the new school year. There will be JDwags and over 300 dollars in door prizes!'
   },
   {
-    title: 'Resume/Hackerrank Workshop',
+    title: 'Resume/Hackerrank Workshop with Goldman Sachs',
     start: moment('Sep 17 2019 06:00pm', 'MMM DD YYYY hh:mma'),
-    end: moment('Sep 17 2019 07:00pm', 'MMM DD YYYY hh:mma'),
+    end: moment('Sep 17 2019 08:00pm', 'MMM DD YYYY hh:mma'),
     location: 'TBD',
-    description: 'Hosted by Goldman Sachs'
+    description: 'Bring your resume and be ready to prep yourself for interviews.'
   },
   {
-    title: 'AIS/ACM Social',
+    title: 'AIS/ACM Social with Pariveda',
     start: moment('Sep 20 2019 05:00pm', 'MMM DD YYYY hh:mma'),
     end: moment('Sep 20 2019 07:00pm', 'MMM DD YYYY hh:mma'),
     location: 'Good Move Cafe (1 E. Center St. Suite 100 Provo, UT 84601)',
-    description: 'Hosted by Pariveda'
+    description: 'Come to the Good Move Cafe for free food and games! Bring your significant others and be ready to network and have fun.'
   },
   {
     title: 'Recruitment Dinner',
@@ -36,7 +36,7 @@ const events = [
     start: moment('Sep 25 2019 09:00am', 'MMM DD YYYY hh:mma'),
     end: moment('Sep 26 2019 03:00pm', 'MMM DD YYYY hh:mma'),
     location: 'WSC Ballroom',
-    description: 'Meet with recruiters from 9am to 3pm. Hand out your resume and start networking!'
+    description: 'Meet with recruiters from 9am to 3pm. Hand out your resume and start networking! Even if you are not looking for a job/internship yet, go and meet people. See what recruiters expect in students and start building relationships.'
   },
   {
     title: 'Hallo',
