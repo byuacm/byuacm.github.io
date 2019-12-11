5218c9d19d4b5435ec908acf188240174b465232
edenknudson <35382012+edenknudson@users.noreply.github.com>
2019-12-10 17:19:48 -0700

    Update resources.js

--- src/constants/resources.js
+++ src/constants/resources.js
@@ -6,6 +6,11 @@ const resources = [
     description: 'Qualtrics presentation given in Winter 2019',
     link: '/static/resources/qualtircsInterview.pdf',
   },
+  {
+    title: 'Technical Interview Tips',
+    description: 'Tech Interview Handbook · Carefully curated content to help you ace your next technical interview',
+    link: 'https://yangshun.github.io/tech-interview-handbook/introduction',
+  },
 ];
 
 export default resources;
