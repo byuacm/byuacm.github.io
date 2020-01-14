e30479a8d060afe7cf66074b9fc257cd3f12d185
edenknudson <35382012+edenknudson@users.noreply.github.com>
2020-01-14 10:22:32 -0700

    Update HomePage.vue

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -2,18 +2,11 @@
   <div v-if="semester !== ''" class="home-container">
     <md-card>
       <md-card-header>
-        <!-- <div class="md-title">Next club activity for {{semester}} semester:</div> -->
-        <div class="md-title">Happy Holidays!</div>
+        <div class="md-title">Next club activity for {{semester}} semester:</div>
       </md-card-header>
-      <md-card-content>
-        <a href="https://www.adventofcode.com">
-          <h2>Advent of Code!</h2>
-        </a>
-        <p>Compete against other BYU students by participating in Advent of Code. This is an Advent calendar of small programming puzzles for a variety of skill sets and skill levels that can be solved in any programming language you like. The top 5 BYU students will get a free t-shirt at the ACM opening social for Winter 2020 semester! Use the access code <b>823745-b1c327c3</b> to join BYU's  <a href="https://adventofcode.com/2019/leaderboard/private">private leaderboard</a>. You must have an account to join a private leaderboard.</p>
-      </md-card-content>
       <md-card-media>
         <!-- <a href="https://www.adventofcode.com"> -->
-          <!-- <img class="flyer" v-bind:src="flyer"> -->
+        <img class="flyer" v-bind:src="flyer">
         <!-- </a> -->
       </md-card-media>
     </md-card>
