0c0b0bf24d33e8be93b8cc9b58275cb1a45e3dfc
Eden Knudson <edenknudson@Edens-MBP.app.byu.edu>
2020-02-17 14:33:41 -0700

    Added new info flyer for hackathon

--- src/components/Hackathon.vue
+++ src/components/Hackathon.vue
@@ -9,26 +9,12 @@
           <img class="flyer" v-bind:src="flyer">
         </a>
       </md-card-media>
+      <md-card-media>
+        <a href="https://byu.az1.qualtrics.com/jfe/form/SV_aVmeFBusXsnw1tH">
+          <img class="info_flyer" v-bind:src="info_flyer">
+        </a>
+      </md-card-media>
       <md-card-content>
-        <h2>Here's why this year's Y-Hack is going to be fun</h2>
-        <ol>
-          <li>Fun and engaging workshops: Reinforcement Learning, Flutter, Rust and so much more</li>
-          <li>Network with companies</li>
-          <li>Great hackathon mentors: there’ll be computer science faculty and other students to help you get started on your hack</li>
-          <li>Great food</li>
-          <li>Cool prizes</li>
-          <li>Fun games and challenges</li>
-          <li>Make meaningful connections with other students</li>
-        </ol>
-        <h2>Still on the fence about hackathons in general? Here are some reasons to attend hackathons:</h2>
-        <ol>
-          <li>You get a chance to put your creative hat on and finally work on that crazy idea you’ve had</li>
-          <li>Work with and learn from other students</li>
-          <li>It’s just fun to be in a place where everyone’s just hacking at something, learning and having fun together</li>
-          <li>Network. Can’t say enough about this one</li>
-          <li>Well, hackathons are just outright fun!</li>
-          <li>Great products have come out of hackathons: one of our own is Nick Walton’s <a href="https://aidungeon.io">AI Dungeon 2</a> Others include GroupMe, EasyTaxi, Carousell</li>
-        </ol>
         <h2 class="signup">
           <a href="https://byu.az1.qualtrics.com/jfe/form/SV_aVmeFBusXsnw1tH">Sign up for the Y-Hack!</a>
         </h2>
@@ -45,6 +31,7 @@ export default {
     return {
       semester: "Winter 2020", // Always change every semester
       flyer: "../static/hackathon_flyer.jpg", // DON'T change! Make sure the flyer has the same file name and extention.
+      info_flyer: "../static/hackathon_info_flyer.jpg"
     };
   },
 };
